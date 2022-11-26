e5a738adae4b189d59f346cb61ebe239ec4c7fa9
unleashedbest <linvestigacion4@gmail.com>
2022-11-26 20:45:40 -0300

    Requirements update

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='A short description of the project.',
-    author='Miguel',
-    license='MIT',
+    description='The purpose of this project is generate a ML stacking model that predict the risk of diabetes on a certain population, also, we what to show diferent habilities in data science, such us Mining, Wrangling, Cleaning, Dataset description, ML training, ML analisis.',
+    author='Hernan',
 )
